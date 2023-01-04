35ca717405221384dc40f4b98848b95079e789f0
Amoljamnik97 <ajamnik.mech@gmail.com>
2023-01-04 10:41:21 +0530

    [amol]- Solving Snak And Ladder Game

---
+++ SnakeAndLadders/SnakeAndLadders/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1=new Player();
+            player1.RolleDice(0, 7);
+        }
+    }
+}
